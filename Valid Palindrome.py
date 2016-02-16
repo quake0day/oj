4505c7e7403cec14d7c5e85c642e1bc978269157
quake0day <quake0day@gmail.com>
2016-02-16 02:51:09 -0500

    a
    a

---
+++ Valid Palindrome.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        start = 0
+        end = len(s) - 1
+        while start < end:
+            while start < end and not s[start].isalnum():
+                start += 1
+            while start < end and not s[end].isalnum():
+                end -= 1
+            if s[start].lower() != s[end].lower():
+                return False
+            start += 1
+            end -= 1
+        return True
+
+a = Solution()
+print a.isPalindrome("A man, a plan, a canal: Panama")
+print a.isPalindrome(".,")
